17d43a01f74555bd79e68bb18cce24893c8c81a5
April <lemongrabssister@gmail.com>
2023-08-17 23:05:20 +0100

    started writing a test, page 70

---
+++ taco-cloud/src/test/java/tacos/HomeControllerTests.java
@@ -0,0 +1,16 @@
+package tacos;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import tacos.controller.HomeController;
+
+@WebMvcTest(HomeController.class)
+public class HomeControllerTests {
+}
